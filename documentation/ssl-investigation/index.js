124879f87f4163628a9b701138ade5b439069624
JamesBowers <james_m_bowers@icloud.com>
2017-11-05 16:15:16 +0000

    client key now .p12

--- documentation/ssl-investigation/index.js
+++ documentation/ssl-investigation/index.js
@@ -8,38 +8,40 @@ const pem = {
   createCertificate: Promise.promisify(p.createCertificate),
 }
 
-const express = require('express')
-const cookieParser = require('cookie-parser')
-const app = express()
-var bodyParser = require('body-parser')
-app.use(bodyParser.json())
-app.use(cookieParser())
-app.use(bodyParser.urlencoded({ extended: false }))
-
 const privateRsaKey = require('./settings').privateRsaKey
 
+const express = require('express')
+const app = express()
+
 app.get('/', (req, res) => {
-  let emailAddress = req.query.emailAddress;
+  var cert = req.connection.getPeerCertificate();
+  res.send('Welcome\n Cert details are:' + JSON.stringify(cert,null,2))
+})
 
+app.get('/getP12', (req, res) => {
+  let emailAddress = req.query.emailAddress || 'james@ticketbuddy.co.uk';
   pem.createCSR({emailAddress})
-  .then(csr => pem.createCertificate({csr: csr.csr, serviceKey: privateRsaKey, selfSigned:true}))
-  .then(certificate => {
-    open('Your certificate is: \n\n', certificate.certificate);
-    p.checkCertificate(certificate.certificate, (err, valid) => {
-      res.send(`Check the logs for your certificate.`);
+  .then(csr => pem.createCertificate({days: 100, csr: csr.csr, serviceKey: privateRsaKey, selfSigned:true}))
+  .then(pem => {
+    p.createPkcs12(pem.serviceKey, pem.certificate, 'chicken', {/* options */}, (err, pkcs12StringBuffer) => {
+      res.writeHead(200, {
+          'Content-disposition': 'attachment;filename=core-io.p12',
+      });
+      res.end(pkcs12StringBuffer.pkcs12);
     })
   })
 })
 
-app.post('/', (req, res) => {
-  let certificate = req.body.certificate;
-  p.checkCertificate(certificate, (err, valid) => {
-    p.readCertificateInfo(certificate, (err, info) => {
-      res.send(JSON.stringify(info,null,2));
-    })
-  })
-})
+// create https server
+const https = require('https');
 
-app.listen(3000, function () {
-  console.log('Certificate generating app listening on port 3000!')
+p.createCertificate({ days: 100, selfSigned: true, serviceKey:privateRsaKey }, (err, keys) => {
+  if (err) throw err
+
+  https.createServer({
+    key: keys.serviceKey,
+    cert: keys.certificate,
+    requestCert: true,
+    rejectUnauthorized: false
+  }, app).listen(3000)
 })
