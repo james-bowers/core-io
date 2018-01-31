fb98685f9f5043ca1c170eb8ff8afc9d15d97f14
JamesBowers <james_m_bowers@icloud.com>
2018-01-31 17:53:33 +0000

    GCP credentials

--- backend-server-system/src/cloud-resource-library/services/gcpSDK.js
+++ backend-server-system/src/cloud-resource-library/services/gcpSDK.js
@@ -3,10 +3,7 @@ let cloudAccessCredentials = require('./cloud-credentials.json')
 
 module.exports = (service) => {
     
-    // to specify for different account access: https://github.com/GoogleCloudPlatform/nodejs-docs-samples/blob/master/auth/auth.js#L59
-    // otherwise use the same account as core-io is in
-
     return {
-        // Storage: Storage()
+        Storage: Storage(cloudAccessCredentials.gcp)
     }[service]
 }
