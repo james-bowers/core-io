de4e4d9a99814ec7091651d390c43beb92b57b1a
JamesBowers <james_m_bowers@icloud.com>
2018-02-06 00:50:50 +0000

    gcp cloudfunc working well

--- backend-server-system/src/cloud-resource-library/services/Serverless/gcp/deploy.js
+++ backend-server-system/src/cloud-resource-library/services/Serverless/gcp/deploy.js
@@ -1,38 +1,37 @@
 const Promise = require('bluebird'),
-    helper = require('./../../../helper')
-    // uploadCloudFunc = require('./uploadCloudFunc').usingBuffer,
-    // deleteFunction = require('./delete'),
-    // createFunction = require('./create')
+    helper = require('./../../../helper'),
+    uploadCloudFunc = require('./uploadCloudFunc').usingBuffer
 
 module.exports = (gcp, configuration, resource, gcpRegion, tagName, options) => {
 
-    return require('./update')(gcp, configuration, resource, gcpRegion, tagName, options)
+    let { authClient, cloudFunctions } = gcp('CloudFunctions')
 
-    // let { authClient, cloudFunctions } = gcp('CloudFunctions')
+    let readableRegion = helper.getReadableRegion(resource, gcpRegion)
+    let functionId = resource.cloudVendorInformation[readableRegion].functionId
 
-    // let readableRegion = helper.getReadableRegion(resource, gcpRegion)
-    // let functionId = resource.cloudVendorInformation[readableRegion].functionId
+    let location = `projects/deployments-project/locations/${gcpRegion}`
 
+    return uploadCloudFunc(gcp, options.zipBuffer, functionId, gcpRegion)
+        .then(pathToZip => {
 
-    // // upload new func
-    // return uploadCloudFunc(gcp, options.zipBuffer, functionId, gcpRegion).then((pathToZip) => {
-    
-    //     // delete the old
-    //     return Promise.delay(5000)
-    //     .then(() => deleteFunction(gcp, gcpRegion, functionId))
-    //     .then(() => {
-    //         // create the new
-    //         return createFunction(gcp, configuration, resource, gcpRegion, tagName, { functionId })
-    //     })
+            return Promise.fromCallback(function (callback) {
+                let params = {
+                    auth: authClient,
+                    name: `${location}/functions/${functionId}`,
+                    resource: {
+                        name: `${location}/functions/${functionId}`,
+                        entryPoint: 'main',
+                        sourceArchiveUrl: `gs://${pathToZip}`,
+                        httpsTrigger: {},
+                        availableMemoryMb: 128
+                    }
+                }
 
-    // }).then(result => {
-    //     return { functionId }
-    // })
+                return cloudFunctions.projects.locations.functions.update(params, callback)
+            })
+
+        }).then(result => {
+            return { functionId }
+        })
 
 }
-
-
-
-
-
-
